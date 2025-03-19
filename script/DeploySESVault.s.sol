4a0e88464036e726b4979a9bd67f8eadc20de7c8
George Gorzhiyev <gegorz@hotmail.com>
2025-03-18 20:53:37 -0600

    Created simple deployment script for the SESVault

--- script/DeploySESVault.s.sol
+++ script/DeploySESVault.s.sol
@@ -2,12 +2,14 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "../forge-std/Script.sol";
-import {SimpleEthereumStorageVault} from "../src/SimpleEthereumStorageVault.sol";
+import {Script} from "forge-std/Script.sol";
+import {SimpleEthereumStorageVault} from "src/SimpleEthereumStorageVault.sol";
 
 contract DeploySESVault is Script {
     function run() external returns (SimpleEthereumStorageVault) {
+        vm.startBroadcast();
         SimpleEthereumStorageVault sesv = new SimpleEthereumStorageVault();
+        vm.stopBroadcast();
         return sesv;
     }
 }
