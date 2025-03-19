e0df4edf23f52293e11bdd122aa7c2ac0a9f44eb
George Gorzhiyev <gegorz@hotmail.com>
2025-03-19 17:54:56 -0600

    Deployment script also uses Helper Config to get price feed based on chain id

--- script/DeploySESVault.s.sol
+++ script/DeploySESVault.s.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {SimpleEthereumStorageVault} from "src/SimpleEthereumStorageVault.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeploySESVault is Script {
     function run() external returns (SimpleEthereumStorageVault) {
+        HelperConfig helperConfig = new HelperConfig();
+        address priceFeed = helperConfig.getConfigByChainId(block.chainid).priceFeed;
+
         vm.startBroadcast();
-        SimpleEthereumStorageVault sesv = new SimpleEthereumStorageVault();
+        SimpleEthereumStorageVault sesv = new SimpleEthereumStorageVault(priceFeed);
         vm.stopBroadcast();
         return sesv;
     }
