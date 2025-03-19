af941afd2b3041eb49ed9d9f6305b28860bd5e4b
George Gorzhiyev <gegorz@hotmail.com>
2025-03-19 13:09:06 -0600

    Expanded withdraw function and added some event emitters

--- src/SimpleEthereumStorageVault.sol
+++ src/SimpleEthereumStorageVault.sol
@@ -29,16 +29,21 @@ pragma solidity ^0.8.18;
  * @notice A simple Ethereum vault where users can deposit/withdraw Ethereum. Check balance of their account along with vault and check the USD value of their deposited Ethereum.
  */
 contract SimpleEthereumStorageVault {
-    /*==================== Errors ====================*/
+    /*==================== ERRORS =============================*/
     error SimpleEthereumStorageVault__FailedToWithdrawEthereum();
     error SimpleEthereumStorageVault__MustBeMoreThanZero();
     error SimpleEthereumStorageVault__WithdrawalMoreThanBalance();
+    error SimpleEthereumStorageVault__UserHasNoBalance();
 
-    /*==================== State Variables ====================*/
+    /*==================== STATE VARIABLES ====================*/
     address private immutable i_vaultOwner;
     mapping(address user => uint256 balance) private s_userBalances;
 
-    /*==================== Modifiers ====================*/
+    /*==================== EVENTS =============================*/
+    event DepositedEthereum(address indexed user, uint256 amount);
+    event WithdrawalEthereum(address indexed user, uint256 amount);
+
+    /*==================== MODIFIERS ==========================*/
     modifier moreThanZero(uint256 amount) {
         if (amount == 0) {
             revert SimpleEthereumStorageVault__MustBeMoreThanZero();
@@ -46,7 +51,7 @@ contract SimpleEthereumStorageVault {
         _;
     }
 
-    /*==================== Functions ====================*/
+    /*==================== FUNCTIONS ==========================*/
     /**
      * @notice Constructor to set the owner of the vault
      */
@@ -55,27 +60,36 @@ contract SimpleEthereumStorageVault {
     }
 
     /**
-     * @notice A payable function to store Ethereum in the vault
+     * @notice A payable function to store Ethereum in the vault contract
      */
     function storeEthereum() public payable {
         require(msg.value > 0, "Cannot store 0 Ethereum");
         s_userBalances[msg.sender] += msg.value;
+
+        emit DepositedEthereum(msg.sender, msg.value);
     }
 
     /**
      * @param amount Amount of Ethereum to withdraw
+     *
+     * Uses CEI - Checks Effects Interactions pattern
      */
     function withdrawEthereum(uint256 amount) public moreThanZero(amount) {
         uint256 userBalance = getUserBalance(msg.sender);
         if (amount > userBalance) {
             revert SimpleEthereumStorageVault__WithdrawalMoreThanBalance();
+        } else if (userBalance == 0) {
+            revert SimpleEthereumStorageVault__UserHasNoBalance();
         }
+
+        s_userBalances[msg.sender] -= amount;
 
         (bool success,) = msg.sender.call{value: amount}("");
         if (!success) {
             revert SimpleEthereumStorageVault__FailedToWithdrawEthereum();
         }
-        s_userBalances[msg.sender] -= amount;
+
+        emit WithdrawalEthereum(msg.sender, amount);
     }
 
     /**
